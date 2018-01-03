9db6b4682d38337e924c7bce3972004528db2848
johnrudell <johnjrudell@gmail.com>
2018-01-03 15:11:56 -0500

    Add SessionForm styling

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -12,11 +12,18 @@ import SessionFormContainer from './session_form/session_form_container';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
-  <div>
-    <header>
-      <h1>The Rubbing Lamp</h1>
+  <div className="box">
+
+    <nav className="row header temp-nav">
+      <ul className="temp-nav-left temp-nav-list">
+        <li>Explore</li>
+        <li>Make a wish</li>
+      </ul>
+
+      <Link className="temp-nav-center" to="/">The Rubbing Lamp</Link>
+
       <GreetingContainer />
-    </header>
+    </nav>
 
     <AuthRoute path="/login" component={SessionFormContainer} />
     <AuthRoute path="/signup" component={SessionFormContainer} />
--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -32,9 +32,9 @@ class SessionForm extends React.Component {
 
   renderErrors() {
     return(
-      <ul>
-        {this.props.errors.map((error, i) => (
-          <li key={i}>
+      <ul className="session-errors">
+        {this.props.errors.map((error, idx) => (
+          <li key={idx}>
             {error}
           </li>
         ))}
@@ -47,35 +47,44 @@ class SessionForm extends React.Component {
     if (this.props.formType === 'signup') {
       // Sign Up
       return (
-        <div className="login-form-container">
-          <form className="login-form-box" onSubmit={this.handleSubmit}>
-            <p>Have an account? <Link to="/login">Log in</Link></p>
-            <h2>Sign Up</h2>
-            <div className="login-form">
-              <label>Username:
-                <input className="signup-input"
-                  type="text"
-                  value={this.state.username}
-                  onChange={this.update('username')} />
-              </label>
-              <br />
-              <label>Email:
-                <input className="signup-input"
-                  type="text"
-                  onChange={this.update('email')} />
-              </label>
-              <br />
-              <label>Password:
-                <input className="signup-input"
-                  type="password"
-                  value={this.state.password}
-                  onChange={this.update('password')} />
-              </label>
-              <br />
-              <input className="submit-button"
+        <div className="row content session-form-container">
+          <form className="session-form-box" onSubmit={this.handleSubmit}>
+            <div className="session-form-addon session-form-header">
+              Have an account? <Link className="link" to="/login">Log in</Link>
+            </div>
+            <h2>Sign up</h2>
+            {this.renderErrors()}
+            <div className="session-form">
+              <input className="session-input"
+                placeholder="Name"
+                type="text"
+                value={this.state.username}
+                onChange={this.update('username')} />
+              <input className="session-input"
+                placeholder="Email"
+                type="text"
+                onChange={this.update('email')} />
+              <input className="session-input"
+                placeholder="Password"
+                type="password"
+                value={this.state.password}
+                onChange={this.update('password')} />
+              <input className="session-input submit-button"
                 type="submit"
-                value="Create Account" />
-              {this.renderErrors()}
+                value="Create account" />
+              <p className="small-text">
+                By signing up, you agree to our <a className="link">terms of use</a>,&nbsp;
+                <a className="link">privacy policy</a>, and &nbsp;
+                <a className="link">cookie policy</a>.
+              </p>
+              <div className="separator">
+                <div className="line"></div>
+                <div className="text">or</div>
+              </div>
+              <input className="session-input submit-button demo-button"
+                type="submit"
+                value="Demo log in" />
+              <p className="small-text">Log in immediately and experience the wonders of being a genie.</p>
             </div>
           </form>
         </div>
@@ -83,31 +92,35 @@ class SessionForm extends React.Component {
     } else {
       // Log in
       return (
-        <div className="login-form-container">
-          <form className="login-form-box" onSubmit={this.handleSubmit}>
+        <div className="row content session-form-container">
+          <form className="session-form-box" onSubmit={this.handleSubmit}>
             <h2>Log in</h2>
-            <div className="login-form">
-              <label>Username:
-                <input className="login-input"
-                  type="text"
-                  value={this.state.username}
-                  onChange={this.update('username')} />
-              </label>
-              <br/>
-              <label>Password:
-                <input className="login-input"
-                  type="password"
-                  value={this.state.password}
-                  onChange={this.update('password')} />
-              </label>
-              <br />
-              <input className="submit-button"
+            {this.renderErrors()}
+            <div className="session-form">
+              <input className="session-input"
+                placeholder="Name"
+                type="text"
+                value={this.state.username}
+                onChange={this.update('username')} />
+              <input className="session-input"
+                placeholder="Password"
+                type="password"
+                value={this.state.password}
+                onChange={this.update('password')} />
+              <input className="session-input submit-button"
                 type="submit"
-                value="Log Me In!" />
-              {this.renderErrors()}
-              <div className="form-footer">
-                New to The Rubbing Lamp? <Link to='/signup'>Sign up!</Link>
+                value="Log me in!" />
+              <div className="separator">
+                <div className="line"></div>
+                <div className="text">or</div>
               </div>
+              <input className="session-input submit-button demo-button"
+                type="submit"
+                value="Demo log in" />
+              <p className="small-text">Log in immediately and experience the wonders of being a genie.</p>
+            </div>
+            <div className="session-form-addon session-form-footer">
+              New to The Rubbing Lamp? <Link className="link" to='/signup'>Sign up!</Link>
             </div>
           </form>
         </div>
