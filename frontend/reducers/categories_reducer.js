7cc9b6d01b066bf61b454db727e7cb905600b1be
johnrudell <johnjrudell@gmail.com>
2018-03-05 18:28:07 -0500

    Remove category display reducer

--- frontend/reducers/categories_reducer.js
+++ frontend/reducers/categories_reducer.js
@@ -7,11 +7,9 @@ const categoryReducer = (state = {}, action) => {
     case RECEIVE_CATEGORIES:
       return action.categories;
     case RECEIVE_CATEGORY:
-    // const newCategory = {[action.category.id]: action.category};
-    // return merge({}, state, newCategory);
+      // fetch that category's projects by their id's in jbuilder
       const category = action.payload.category;
       category.project_ids = action.payload.projects.map(project => project.id);
-
       return merge({}, state, { [category.id]: category });
     default:
       return state;
